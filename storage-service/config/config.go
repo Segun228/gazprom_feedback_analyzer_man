d00117a957e67465a4841de74e651fb36abe705d
Matvey Fedorov <mfgolden@yandex.ru>
2025-09-22 13:21:19 +0300

    init(storage-service): basic storage service with dockefile and config initiation

---
+++ storage-service/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	HTTP struct {
+		Port string `mapstructure:"port"`
+	} `mapstructure:"http"`
+	DB struct {
+		Name     string `mapstructure:"name"`
+		User     string `mapstructure:"user"`
+		Password string `mapstructure:"password"`
+		Port     string `mapstructure:"port"`
+	} `mapstructure:"db"`
+	Kafka struct {
+		Brokers string `mapstructure:"brokers"`
+		Topics  struct {
+		} `mapstructure:"topics"`
+	} `mapstructure:"kafka"`
+}
+
+var Cfg Config
+
+func InitConfig() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath("./api-gateway")
+
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err != nil {
+		slog.Warn("config file not found. Relying on environment variables")
+	}
+
+	if err := viper.Unmarshal(&Cfg); err != nil {
+		slog.Error("unable to decode config into struct", "error", err)
+		os.Exit(1)
+	}
+}
