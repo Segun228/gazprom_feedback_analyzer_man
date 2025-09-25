f7074bbe0eeeac3c149dd127617e7a96ea22ae49
Matvey Fedorov <mfgolden@yandex.ru>
2025-09-25 14:49:20 +0300

    style(storage-service): correcting main table name

--- storage-service/migrations/createProccessedDataTable.go
+++ storage-service/migrations/createProccessedDataTable.go
@@ -12,28 +12,28 @@ func CreateProcessedDataTable(db clickhouse.Conn) {
 	ctx := context.Background()
 
 	var exists uint8
-	err := db.QueryRow(ctx, "EXISTS TABLE proccessed_data").Scan(&exists)
+	err := db.QueryRow(ctx, "EXISTS TABLE processed_data").Scan(&exists)
 	if err != nil {
-		slog.Error("failed to check if proccessed_data table exists", "error", err)
+		slog.Error("failed to check if processed_data table exists", "error", err)
 		os.Exit(1)
 	}
 
 	if exists != 1 {
 		err = db.Exec(ctx, `
-			CREATE TABLE proccessed_data (
-				id UInt64,
+			CREATE TABLE processed_data (
+				id UInt64 DEFAULT rand(),
 				text String,
 				date DateTime,
-				sentiment Int8,
+				sentiment UInt8,
 				tags Array(String)
 			) ENGINE = MergeTree()
 			ORDER BY id
 		`)
 		if err != nil {
-			slog.Error("failed to create proccessed_data table", "error", err)
+			slog.Error("failed to create processed_data table", "error", err)
 			os.Exit(1)
 		}
 
-		slog.Info("proccessed_data table created successfully!")
+		slog.Info("processed_data table created successfully!")
 	}
 }
