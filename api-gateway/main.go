f02a4d392cd32ebe5a802bd2dd45e32008cb7597
Matvey Fedorov <mfgolden@yandex.ru>
2025-10-02 21:28:42 +0300

    fix: deleting commentary

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -55,7 +55,6 @@ func main() {
 	r.Group(func(r chi.Router) {
 		slog.Info("setting up public routes")
 
-		// Редирект с корня на дашборд
 		r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 			http.Redirect(w, r, "/dashboard/", http.StatusMovedPermanently)
 		})
